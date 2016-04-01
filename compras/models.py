50e160337f56852fda340084cd4db2509522997b
Carlos Alejandro Garcia Jimenez <MAT3O@mac-book001.local>
2016-04-01 14:11:04 -0600

    cambios en el modelo models.py

--- compras/models.py
+++ compras/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-           default=timezone.now)
-    published_date = models.DateTimeField(
-           blank=True, null=True)
+class Producto(models.Model):
+    nombre=models.CharField(max_length=200)
+    fechaRegistroInicial=models.DateTimeFild()
+    fechaCompra=models.DateTimeFild(balnk=TRUE, null=True)
 
-    def publish(self):
-        self.published_date = timezone.now()
+
+    def comprar(self):
+        self.fechaCompra=timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.nombre
